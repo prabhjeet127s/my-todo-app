37b058790d5b940bab6b54c1205ba29165d25aec
prabhjet singh <prabhjeet127.s@gmail.com>
2025-07-19 17:52:09 +0530

    created basic UI of todolist

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,33 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+
+  const handleEdit=()=>{
+
+  }
+  const handleDelete=()=>{
+    
+  }
+  const handleAdd=()=>{
+    setTodos([...todos,{todo,iscompletes: false}])
+    console.log(todos);
+    
+    
+  }
+  const handleChange=(e)=>{
+    setTodo(e.target.value)
+    
+    
+  }
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <Navbar/>
+
+    <div className="container mx-auto bg-violet-100 my-5 rounded-xl p-5 min-h-[80vh]">
+
+      <div className="addTodo my-5">
+        <h2 className='text-lg font-bold'>
+          Add a Todo </h2>
+          <input onChange={handleChange}  value={todo} type="text"  className='bg-white w-1/2'/>
+          <button  onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 mx-2 rounded-md text-white font-bold'>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
+        <h2 className='text-lg font-bold' >Your Todos</h2>
+        <div className="todos">
+
+
+          {todos.map((item)=>{
+
+
+       return   <div key={todo} className="todo flex justify-between w-1/4 my-3">
+            <div className="text ">{item.todo}</div>
+            <div className="buttons">
+              <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 mx-1 rounded-md text-white font-bold'>Edit</button>
+              <button  onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 mx-1 rounded-md text-white font-bold '>Delete</button>
+
+            </div>
+
+          </div>
+          })}
+        </div>
+
+     
+
+    </div>
+     
     </>
   )
 }
---
+++ todolist/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    
+
+    <nav className='flex justify-between  bg-indigo-900 text-white py-2'> 
+
+    <div className="logo ">
+        <span className='font-bold text-xl mx-8'>iTasks</span>
+    </div>
+
+    <ul className=' flex gap-8 mx-9'>
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li>Your Tasks</li>
+    </ul>
+       
+    </nav>
+  )
+}
+
+export default Navbar
